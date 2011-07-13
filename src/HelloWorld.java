9b439b5c6c3c58e3652bab8d641ae205e6396668
Richard Achmatowicz <rachmatowicz@jboss.com>
2011-07-13 11:47:18 -0400

    Revision 1 - the HW functionality implemented.

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: nrla
+ * Date: 7/13/11
+ * Time: 11:41 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
